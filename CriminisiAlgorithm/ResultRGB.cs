1b2f3dc9f4baf091293b3d53c683f55d01145ddf
iuli18icl <iuliana.clotan@yahoo.com>
2023-06-14 00:02:19 +0300

    Add Results classes

---
+++ CriminisiAlgorithm/ResultRGB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriminisiAlgorithm
+{
+    internal class ResultRGB
+    {
+        // vectori care contin blocurile finale (binarizate)
+        public int[] RedValues { get; set; }
+        public int[] GreenValues { get; set; }
+        public int[] BlueValues { get; set; }
+        public Operation Operation { get; set; }
+
+        public void SetPixels(int[] RedValues, int[] GreenValues, int[] BlueValues)
+        {
+            this.RedValues = RedValues;
+            this.GreenValues = GreenValues;
+            this.BlueValues = BlueValues;
+        }
+    }
+}
