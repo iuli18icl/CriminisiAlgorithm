bb601888510430ce3f2718080e7619c961a28f1e
iuli18icl <iuliana.clotan@yahoo.com>
2023-06-15 01:04:05 +0300

    Add FuzzyDictionary Class

---
+++ CriminisiAlgorithm/FuzzyDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriminisiAlgorithm
+{
+    internal class FuzzyDictionary
+    {
+        public Dictionary<int, double> fuzzyMembershipComputed;
+        public readonly int a = 4;
+        public readonly int b = 78;
+        public readonly int patchsize = 9;
+    }
+}
